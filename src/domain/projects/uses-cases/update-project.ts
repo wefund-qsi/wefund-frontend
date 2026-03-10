472a3bdd3f117960dbb8f2212969250f024d92fb
Julien-Conoir <julien.conoir.etu@univ-lille.fr>
2026-03-10 21:48:59 +0100

    refactor: update UpdateProject use case to accept Project data instead of ProjectId

--- src/domain/projects/uses-cases/update-project.ts
+++ src/domain/projects/uses-cases/update-project.ts
@@ -1,20 +1,20 @@
 import type { Executable } from "../../../shared/executable";
-import { ProjectId, type Project } from "../entities/project";
+import type { Project } from "../entities/project";
 import { ProjectNotFoundException } from "../exceptions/project-not-found";
 import type { IProjectRepository } from "../ports/project-repository.interface";
 
-export class UpdateProject implements Executable<ProjectId, Project> {
+export class UpdateProject implements Executable<Project, Project> {
     private readonly projectRepository: IProjectRepository;
     
     constructor(projectRepository: IProjectRepository) {
         this.projectRepository = projectRepository;
     }
 
-    async execute(id: ProjectId): Promise<Project> {
-        const project = await this.projectRepository.findById(id);
+    async execute(data: Project): Promise<Project> {
+        const project = await this.projectRepository.update(data);
 
         if (!project) {
-            throw new ProjectNotFoundException(id);
+            throw new ProjectNotFoundException(data.id);
         }
 
         return project;
