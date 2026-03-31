b76cfe9fa11ec387d8c734c2ef64a5b68e5386c6
Alyocha <alyocha.yahi.etu@univ-lille.fr>
2026-03-31 12:53:54 +0200

    documentation campaign-not-active.ts

--- src/domain/contributions/exceptions/campaign-not-active.ts
+++ src/domain/contributions/exceptions/campaign-not-active.ts
@@ -1,4 +1,12 @@
+/**
+ * Exception levée quand on tente de contribuer à une campagne inactive
+ *
+ * Utilisée lorsqu'un utilisateur tente de financer une campagne qui n'est pas en statut ACTIVE.
+ */
 export class CampaignNotActiveException extends Error {
+  /**
+   * Constructeur de l'exception
+   */
   constructor() {
     super("Only active campaigns can be funded.");
     this.name = "CampaignNotActiveException";
