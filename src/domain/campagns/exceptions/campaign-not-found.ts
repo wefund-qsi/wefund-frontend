810fc7506d6945fba4ab300456af6ad588b58971
Alyocha <alyocha.yahi.etu@univ-lille.fr>
2026-03-31 12:38:29 +0200

    documentation campaign-not-found.ts

--- src/domain/campagns/exceptions/campaign-not-found.ts
+++ src/domain/campagns/exceptions/campaign-not-found.ts
@@ -1,6 +1,16 @@
 import type { CampaignId } from "../entites/campaign";
 
+/**
+ * Exception levée quand une campagne n'est pas trouvée
+ *
+ * Utilisée lorsqu'on tente d'accéder à une campagne qui n'existe pas dans le système.
+ */
 export class CampaignNotFoundException extends Error {
+    /**
+     * Constructeur de l'exception
+     *
+     * @param {CampaignId} id - L'ID de la campagne qui n'a pas été trouvée
+     */
     constructor(id: CampaignId) {
         super(`Campaign with id "${id}" was not found.`);
         this.name = "CampaignNotFoundException";
