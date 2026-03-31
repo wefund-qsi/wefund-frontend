ae908cc0deb3b6bae13527ab7a326def406e2145
Alyocha <alyocha.yahi.etu@univ-lille.fr>
2026-03-31 12:51:31 +0200

    view-project-campaigns.ts

--- src/domain/campagns/uses-cases/view-project-campaigns.ts
+++ src/domain/campagns/uses-cases/view-project-campaigns.ts
@@ -3,6 +3,11 @@ import type { ProjectId } from "../../projects/entities/project";
 import type { Campaign } from "../entites/campaign";
 import type { ICampaignRepository } from "../ports/campaign-repository.interface";
 
+/**
+ * Use-case pour récupérer toutes les campagnes d'un projet
+ *
+ * Récupère la liste de toutes les campagnes associées à un projet spécifique.
+ */
 export class ViewProjectCampaigns implements Executable<ProjectId, Campaign[]> {
     private readonly campaignRepository: ICampaignRepository;
 
