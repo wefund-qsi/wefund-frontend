23331c49757a18fa16aeb8c6a03977845e2edfb4
Alyocha <alyocha.yahi.etu@univ-lille.fr>
2026-03-31 12:55:03 +0200

    documentation contribution-not-found.ts

--- src/domain/contributions/exceptions/contribution-not-found.ts
+++ src/domain/contributions/exceptions/contribution-not-found.ts
@@ -1,4 +1,12 @@
+/**
+ * Exception levée quand une contribution n'est pas trouvée
+ *
+ * Utilisée lorsqu'on tente d'accéder à une contribution qui n'existe pas dans le système.
+ */
 export class ContributionNotFoundException extends Error {
+  /**
+   * Constructeur de l'exception
+   */
   constructor() {
     super("Contribution not found.");
     this.name = "ContributionNotFoundException";
