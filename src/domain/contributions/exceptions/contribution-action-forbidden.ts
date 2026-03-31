25a1215d072abc84f2fe4278ae900196cd69251a
Alyocha <alyocha.yahi.etu@univ-lille.fr>
2026-03-31 12:54:32 +0200

    documentation contribution-action-forbidden.ts

--- src/domain/contributions/exceptions/contribution-action-forbidden.ts
+++ src/domain/contributions/exceptions/contribution-action-forbidden.ts
@@ -1,6 +1,16 @@
+/**
+ * Exception levée quand une action interdite est tentée sur une contribution
+ *
+ * Utilisée lorsqu'on tente de modifier ou rembourser une contribution
+ * alors que la campagne n'est pas active.
+ */
 export class ContributionActionForbiddenException extends Error {
+  /**
+   * Constructeur de l'exception
+   */
   constructor() {
     super("This contribution can only be modified or refunded while the campaign is active.");
     this.name = "ContributionActionForbiddenException";
   }
 }
+
