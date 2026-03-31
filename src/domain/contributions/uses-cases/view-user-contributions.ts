027c570bfb6bf87d3ac00f44ffac02a8c6cf3048
Alyocha <alyocha.yahi.etu@univ-lille.fr>
2026-03-31 13:08:02 +0200

    update-contribution.ts

--- src/domain/contributions/uses-cases/view-user-contributions.ts
+++ src/domain/contributions/uses-cases/view-user-contributions.ts
@@ -3,9 +3,19 @@ import type { UserId } from "../../users/entities/user";
 import type { Contribution } from "../entities/contribution";
 import type { IContributionRepository } from "../ports/contribution-repository.interface";
 
+/**
+    * Use-case pour afficher les contributions d'un utilisateur.
+    *
+    * Ce use-case récupère toutes les contributions associées à l'identifiant de l'utilisateur donné.
+    */
 export class ViewUserContributions implements Executable<UserId, Contribution[]> {
   private readonly contributionRepository: IContributionRepository;
 
+/**
+    * @param contributionRepository - Le repository pour accéder aux contributions
+    *
+    * Ce constructeur initialise le repository nécessaire pour exécuter le use-case d'affichage des contributions d'un utilisateur.
+    */
   constructor(contributionRepository: IContributionRepository) {
     this.contributionRepository = contributionRepository;
   }
