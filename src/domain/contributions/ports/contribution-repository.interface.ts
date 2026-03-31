eb23a4d1977730a8572bfcc2d82070b98547d2cf
Alyocha <alyocha.yahi.etu@univ-lille.fr>
2026-03-31 12:55:44 +0200

    documentation contribution-repository.interface.ts

--- src/domain/contributions/ports/contribution-repository.interface.ts
+++ src/domain/contributions/ports/contribution-repository.interface.ts
@@ -3,11 +3,37 @@ import type { UserId } from "../../users/entities/user";
 import type { Contribution } from "../entities/contribution";
 import type { ContributionId } from "../entities/contribution";
 
+/**
+ * Port pour les opérations sur les contributions
+ */
 export interface IContributionRepository {
+  /**
+   * Crée une nouvelle contribution
+   */
   create(contribution: Contribution): Promise<Contribution>;
+
+  /**
+   * Met à jour une contribution existante
+   */
   update(contribution: Contribution): Promise<Contribution>;
+
+  /**
+   * Supprime une contribution par son ID
+   */
   delete(id: ContributionId): Promise<boolean>;
+
+  /**
+   * Recherche une contribution par son ID
+   */
   findById(id: ContributionId): Promise<Contribution | null>;
+
+  /**
+   * Récupère toutes les contributions d'une campagne
+   */
   findByCampaignId(campaignId: CampaignId): Promise<Contribution[]>;
+
+  /**
+   * Récupère toutes les contributions d'un contributeur
+   */
   findByContributorId(contributorId: UserId): Promise<Contribution[]>;
 }
